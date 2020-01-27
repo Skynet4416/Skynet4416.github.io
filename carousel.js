984746e1db4d2556742dc6bcf734544909a1e113
tamirco2003 <tamir.cohen835@gmail.com>
2020-01-27 18:13:54 +0200

    Added flickity

--- carousel.js
+++ carousel.js
@@ -1,8 +1,4 @@
-let slideIndex = 0;
-
-function goToSlide(e, index) {
-    let images = e.parentElement.querySelectorAll(".carousel-image");
-    images[slideIndex].className = "carousel-image";
-    slideIndex = (index + images.length) % images.length;
-    images[slideIndex].className = "carousel-image active";
-}
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    wrapAround: true,
+});
